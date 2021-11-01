a2be51c10c69087be19f73730a3719cd7a160005
Walace Nascimento <wlacens@hotmail.com>
2021-11-01 08:52:00 -0300

    Atualizando o package jason

--- desafio2.sql
+++ desafio2.sql
@@ -1,5 +1,5 @@
-USE SpotifyClone;
-CREATE VIEW estatisticas_musicais AS SELECT
+CREATE VIEW estatisticas_musicais AS
+SELECT
 (SELECT COUNT(musica_id) FROM musica) AS cancoes,
 (SELECT COUNT(artista_id) FROM artista) AS artistas,
 (SELECT COUNT(album_id) FROM albums) AS albuns;
