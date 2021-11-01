4d9c9757896120cced79f93495dd2b97f328c3c8
Walace Nascimento <wlacens@hotmail.com>
2021-11-01 20:11:03 -0300

    Tentando resolver o requisito 6

---
+++ desafio6.sql
@@ -0,0 +1,12 @@
+CREATE VIEW faturamento_atual AS 
+SELECT
+ROUND(MIN(preco),2) AS faturamento_minimo,
+ROUND(MAX(preco),2) AS faturamento_maximo,
+ROUND(AVG(preco),2) AS faturamento_medio,
+ROUND(SUM(preco),2) AS faturamento_total
+FROM SpotifyClone.usuario AS U
+INNER JOIN SpotifyClone.plano AS P
+ON U.plano_id = P.plano_id;
+
+-- SELECT * FROM faturamento_atual;
+ 
